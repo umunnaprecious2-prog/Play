d9da5f2bde161f18c809bceb7f2e676fa9b296da
Precious Umunna <umunnaprecious2@gmail.com>
2026-07-31 18:28:13 +0100

    Setup Express, Prisma and PostgreSQL with Docker

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({
+  res.status(200).json({
+    success: true,
     message: "Welcome to Play API 🚀",
   });
 });
